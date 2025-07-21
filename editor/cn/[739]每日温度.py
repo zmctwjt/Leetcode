5244d5569f726b326861effca41228671da84de5
zmctwjt <3239759273@qq.com>
2025-07-21 17:44:55 +0800

    默认变更列表

---
+++ editor/cn/[739]每日温度.py
@@ -0,0 +1,42 @@
+# 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现
+# 在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。 
+# 
+#  
+# 
+#  示例 1: 
+# 
+#  
+# 输入: temperatures = [73,74,75,71,69,72,76,73]
+# 输出: [1,1,4,2,1,1,0,0]
+#  
+# 
+#  示例 2: 
+# 
+#  
+# 输入: temperatures = [30,40,50,60]
+# 输出: [1,1,1,0]
+#  
+# 
+#  示例 3: 
+# 
+#  
+# 输入: temperatures = [30,60,90]
+# 输出: [1,1,0] 
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= temperatures.length <= 10⁵ 
+#  30 <= temperatures[i] <= 100 
+#  
+# 
+#  Related Topics 栈 数组 单调栈 👍 1994 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
