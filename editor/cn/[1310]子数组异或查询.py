2bcd04477fc2059687eb0dc113f19e72eee27e9c
zmctwjt <3239759273@qq.com>
2025-07-26 09:12:04 +0800

    默认变更列表

---
+++ editor/cn/[1310]子数组异或查询.py
@@ -0,0 +1,60 @@
+# 有一个正整数数组 arr，现给你一个对应的查询数组 queries，其中 queries[i] = [Li, Ri]。 
+# 
+#  对于每个查询 i，请你计算从 Li 到 Ri 的 XOR 值（即 arr[Li] xor arr[Li+1] xor ... xor arr[Ri]）作为
+# 本次查询的结果。 
+# 
+#  并返回一个包含给定查询 queries 所有结果的数组。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  
+# 输入：arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+# 输出：[2,7,14,8] 
+# 解释：
+# 数组中元素的二进制表示形式是：
+# 1 = 0001 
+# 3 = 0011 
+# 4 = 0100 
+# 8 = 1000 
+# 查询的 XOR 值为：
+# [0,1] = 1 xor 3 = 2 
+# [1,2] = 3 xor 4 = 7 
+# [0,3] = 1 xor 3 xor 4 xor 8 = 14 
+# [3,3] = 8
+#  
+# 
+#  示例 2： 
+# 
+#  
+# 输入：arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+# 输出：[8,0,4,4]
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= arr.length <= 3 * 10^4 
+#  1 <= arr[i] <= 10^9 
+#  1 <= queries.length <= 3 * 10^4 
+#  queries[i].length == 2 
+#  0 <= queries[i][0] <= queries[i][1] < arr.length 
+#  
+# 
+#  Related Topics 位运算 数组 前缀和 👍 185 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from operator import xor
+from typing import List
+from itertools import accumulate
+
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        perfix_xor = list(accumulate(arr, xor,initial=0))
+        return [perfix_xor[l]^perfix_xor[r+1] for l,r in queries]
+        
+# leetcode submit region end(Prohibit modification and deletion)
