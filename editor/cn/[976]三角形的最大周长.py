b1d47644a2bbf89c8d98c1ae4039ccd668ad5c56
zmctwjt <3239759273@qq.com>
2025-08-07 08:52:58 +0800

    默认变更列表

--- editor/cn/[976]三角形的最大周长.py
+++ editor/cn/[976]三角形的最大周长.py
@@ -38,10 +38,15 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from bisect import bisect_right, bisect_left
 from typing import List
 
 
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-
+        nums.sort(reverse=True)
+        for i in range(len(nums)-2):
+            if nums[i] < nums[i+1]+nums[i+2]:
+                return nums[i]+nums[i+1]+nums[i+2]
+        return 0
 # leetcode submit region end(Prohibit modification and deletion)
