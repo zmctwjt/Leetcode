afa8bbe3f298adc0985e2401d5550e01c3e40843
zmctwjt <3239759273@qq.com>
2025-10-01 20:04:21 +0800

    commit

---
+++ editor/cn/[1717]删除子字符串的最大得分.py
@@ -0,0 +1,72 @@
+# 给你一个字符串 s 和两个整数 x 和 y 。你可以执行下面两种操作任意次。 
+# 
+#  
+#  删除子字符串 "ab" 并得到 x 分。 
+#  
+# 
+#  
+#  比方说，从 "cabxbae" 删除 ab ，得到 "cxbae" 。 
+#  
+#  
+#  删除子字符串"ba" 并得到 y 分。
+#  
+#  比方说，从 "cabxbae" 删除 ba ，得到 "cabxe" 。 
+#  
+#  
+# 
+# 
+#  请返回对 s 字符串执行上面操作若干次能得到的最大得分。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入：s = "cdbcbbaaabab", x = 4, y = 5
+# 输出：19
+# 解释：
+# - 删除 "cdbcbbaaabab" 中加粗的 "ba" ，得到 s = "cdbcbbaaab" ，加 5 分。
+# - 删除 "cdbcbbaaab" 中加粗的 "ab" ，得到 s = "cdbcbbaa" ，加 4 分。
+# - 删除 "cdbcbbaa" 中加粗的 "ba" ，得到 s = "cdbcba" ，加 5 分。
+# - 删除 "cdbcba" 中加粗的 "ba" ，得到 s = "cdbc" ，加 5 分。
+# 总得分为 5 + 4 + 5 + 5 = 19 。 
+# 
+#  示例 2： 
+# 
+#  输入：s = "aabbaaxybbaabb", x = 5, y = 4
+# 输出：20
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= s.length <= 10⁵ 
+#  1 <= x, y <= 10⁴ 
+#  s 只包含小写英文字母。 
+#  
+# 
+#  Related Topics 栈 贪心 字符串 👍 41 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        def get_score(s,x,c):
+            stack = []
+            score = 0
+            for char in s:
+                if stack and stack[-1] == c[0] and char == c[1]:
+                    stack.pop()
+                    score += x
+                    continue
+                stack.append(char)
+            return score,stack
+        if x >= y:
+            ans,s = get_score(s,x,'ab')
+            ans += get_score(s,y,'ba')[0]
+        else:
+            ans,s = get_score(s,y,'ba')
+            ans += get_score(s, x, 'ab')[0]
+        return  ans
+# leetcode submit region end(Prohibit modification and deletion)
