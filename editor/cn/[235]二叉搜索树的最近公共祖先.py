69a92998d670aefc85ccbc29b36b8e24336509d2
zmctwjt <3239759273@qq.com>
2025-11-24 16:34:43 +0800

    commit

--- editor/cn/[235]二叉搜索树的最近公共祖先.py
+++ editor/cn/[235]二叉搜索树的最近公共祖先.py
@@ -44,5 +44,10 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
+
 # leetcode submit region end(Prohibit modification and deletion)
