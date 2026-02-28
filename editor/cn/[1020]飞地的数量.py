70f04fa41b3f1411f69ad4fc5e919b179b3755da
zmctwjt <3239759273@qq.com>
2026-02-28 23:23:21 +0800

    commit

---
+++ editor/cn/[1020]飞地的数量.py
@@ -0,0 +1,64 @@
+# 给你一个大小为 m x n 的二进制矩阵 grid ，其中 0 表示一个海洋单元格、1 表示一个陆地单元格。 
+# 
+#  一次 移动 是指从一个陆地单元格走到另一个相邻（上、下、左、右）的陆地单元格或跨过 grid 的边界。 
+# 
+#  返回网格中 无法 在任意次数的移动中离开网格边界的陆地单元格的数量。 
+# 
+#  
+# 
+#  示例 1： 
+#  
+#  
+# 输入：grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
+# 输出：3
+# 解释：有三个 1 被 0 包围。一个 1 没有被包围，因为它在边界上。
+#  
+# 
+#  示例 2： 
+#  
+#  
+# 输入：grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
+# 输出：0
+# 解释：所有 1 都在边界上或可以到达边界。
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  m == grid.length 
+#  n == grid[i].length 
+#  1 <= m, n <= 500 
+#  grid[i][j] 的值为 0 或 1 
+#  
+# 
+#  Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 304 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        m,n = len(grid),len(grid[0])
+        def dfs(row,col):
+            isTrue = False
+            res = 0
+            if col < 0 or col == n or row < 0 or row == m:
+                return 0,True
+            if grid[row][col] == 1:
+                grid[row][col] = 0
+                res += 1
+                for r,c in ((1,0),(0,1),(-1,0),(0,-1)):
+                    t,tt = dfs(row+r,col+c)
+                    res += t
+                    isTrue = isTrue or tt
+            return res,isTrue
+        res = 0
+        for r,row in enumerate(grid):
+            for c,col in enumerate(row):
+                if col == 1:
+                    t,tt = dfs(r,c)
+                    if not tt:
+                        res += t
+        return res
+# leetcode submit region end(Prohibit modification and deletion)
