464b9721d8d4155678ae7605ef2c2682995f55f1
zmctwjt <3239759273@qq.com>
2026-05-12 08:28:01 +0800

    commit

--- editor/cn/[693]交替位二进制数.py
+++ editor/cn/[693]交替位二进制数.py
@@ -38,9 +38,5 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        s = bin(n)
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                return False
-        return True
+        return n ^ (n >> 1) == (1<<n.bit_length())-1
 # leetcode submit region end(Prohibit modification and deletion)
